19db1c32c3c7be5a9006029f25098e7ae881b8cf
Ernestas <112355556+ILoveEveryone24@users.noreply.github.com>
2025-02-25 22:08:17 +0100

    Update migrations.go

--- migrations.go
+++ migrations.go
@@ -3,14 +3,10 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"embed"
 
 	_ "github.com/lib/pq"
 	"github.com/pressly/goose/v3"
 )
-
-//go:embed sql/schema/*.sql
-var migrations embed.FS
 
 func runMigrations(dbURL string) error {
 	db, err := sql.Open("postgres", dbURL)
@@ -18,8 +14,6 @@ func runMigrations(dbURL string) error {
 		return fmt.Errorf("failed to connect to database: %v", err)
 	}
 	defer db.Close()
-
-	goose.SetBaseFS(migrations)
 
 	if err := goose.Up(db, "sql/schema"); err != nil {
 		return fmt.Errorf("failed to run migrations: %v", err)
